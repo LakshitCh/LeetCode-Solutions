1e3cc67ac82cf9e471e362946206db2b17b9fe59
Lakshit Choudhary <lakshitch.15@gmail.com>
2026-08-29 23:21:11 +0530

    Easy - Array | Time: 0 ms (100%), Space: 20.6 MB (18.1%)

---
+++ Python3/Easy/0001-two-sum/0001-two-sum-08-29-2026-23-21-10.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        a={}
+        for i, j in enumerate(nums):
+            if (target-j) in a:
+                return(a[target-j],i)
+            a[j]=i
