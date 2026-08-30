dc9cdfb55441804eeacd00208942efb95b57d99c
Lakshit Choudhary <lakshitch.15@gmail.com>
2026-08-30 23:52:39 +0530

    Delete Python3/Easy/0001-two-sum directory

--- Python3/Easy/0001-two-sum/0001-two-sum-08-29-2026-23-21-10.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a={}
-        for i, j in enumerate(nums):
-            if (target-j) in a:
-                return(a[target-j],i)
-            a[j]=i
