5613ea3fa09c9a645b6f14350bf87f27bc91dda1
Lakshit Choudhary <lakshitch.15@gmail.com>
2026-09-01 23:57:07 +0530

    Medium - Math | Time: 0 ms (100%), Space: 8.5 MB (83.51%)

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,9 +3,9 @@ public:
     int reverse(int x) {
         long long y = 0;
 
-        while (x != 0) {
-            y = y * 10 + x % 10;
-            x /= 10;
+        while (x!=0) {
+            y = y*10 + x%10;
+            x/=10;
         }
 
         if (y > INT_MAX || y < INT_MIN)
